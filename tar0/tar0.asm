0f3474c0fba70e015223f1dd8cb0f68f7c40a8d6
galilko <galilko62@gmail.com>
2023-03-12 09:41:50 +0200

    update

---
+++ tar0/tar0.asm
@@ -0,0 +1,12 @@
+inputA
+$$$ SELL bread $$$
+13.500000d0
+### BUY cheese ###
+10.600000d0
+inputB
+### BUY chocolate ###
+12.500000d0
+$$$ SELL eggs $$$
+621.00000d0
+TOTAL BUY: 23.100000d0
+TOTAL SELL: 634.50000d0
